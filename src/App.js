abf4985bd6b94724e429828530d6d3868cd8cae8
HouJian <hj1254123@qq.com>
2021-06-27 21:09:59 +0800

    安装配置 redux immutable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { memo } from 'react'
+import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config';
 import routes from './routes'
 import { HashRouter } from 'react-router-dom'
+
+import store from './store/index'
 
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style'
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+
   )
 })
