d78009634ffd85186604414189ae7c0b09f7b0c1
HouJian <hj1254123@qq.com>
2021-07-02 23:14:01 +0800

    将 react-redux 从 connect 重构为 hooks 形式，降低代码复杂度。

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,24 +1,28 @@
 import React, { useEffect } from 'react';
+import { shallowEqual, useSelector, useDispatch } from "react-redux";
+import {getBannerList, getRecommendList} from './store/actionCreators';
+
+
 import Slider from '../../components/slider/';
-import { connect } from "react-redux";
-import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list/';
 import Scroll from '../../baseUI/scroll/index';
 import { Content } from './style';
 
-function Recommend(props) {
-  const { bannerList, recommendList } = props;
+export default React.memo(function Recommend() {
+  const { bannerList, recommendList } = useSelector(state => ({
+    bannerList: state.getIn(['recommend', 'bannerList']),
+    recommendList: state.getIn(['recommend', 'recommendList'])
+  }), shallowEqual)
 
-  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
-    //eslint-disable-next-line
-  }, []);
+    dispatch(getBannerList())
+    dispatch(getRecommendList())
+  }, [dispatch])
 
-  const bannerListJS = bannerList ? bannerList.toJS() : [];
-  const recommendListJS = recommendList ? recommendList.toJS() : [];
+  const bannerListJS = bannerList ? bannerList.toJS() : []
+  const recommendListJS = recommendList ? recommendList.toJS() : []
 
   return (
     <Content>
@@ -30,26 +34,6 @@ function Recommend(props) {
       </Scroll>
     </Content>
   );
-}
+})
 
-// 映射 Redux 全局的 state 到组件的 props 上
-const mapStateToProps = (state) => ({
-  // 不要在这里将数据 toJS
-  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
-  bannerList: state.getIn(['recommend', 'bannerList']),
-  recommendList: state.getIn(['recommend', 'recommendList']),
-});
-// 映射 dispatch 到 props 上
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getBannerDataDispatch() {
-      dispatch(actionTypes.getBannerList());
-    },
-    getRecommendListDataDispatch() {
-      dispatch(actionTypes.getRecommendList());
-    },
-  }
-};
 
-// 将 ui 组件包装成容器组件
-export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
